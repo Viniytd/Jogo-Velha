98b868f982056085cb7c1f3129bf0ebcfbef360d
unknown <viniytd175@gmail.com>
2026-03-13 14:09:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# Jogo da Velha" 
+"# Jogo da Velha" 
+"# Jogo-Velha" 
---
+++ main.js
@@ -0,0 +1,21 @@
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+var i,j,lin,col;
+var jogadas =0;
+var jogador = 'o';
+var vencedor =' ';
+var fimJogo = false;
+var tab = [3];
+//(a)
+while((jogadas<0)&&(fimJogo==false)){
+    //(b)
+    lin = parseInt(prompt(`${jogador} em [linha]: `));
+    col=parseInt(prompt(`${jogador} em [coluna]: `));
+    //(c)
+}
+//(d)
+if(vencedor==' '){
+    console.log("empate\n");
+}else{
+    console.log(`${vencedor} venceu!`);
+}
