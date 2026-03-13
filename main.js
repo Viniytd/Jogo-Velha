ba8d486b7b6407c3b3a34416f1cf2444c457163e
unknown <viniytd175@gmail.com>
2026-03-13 14:33:24 -0300

    Alteracaoes no comment b e c

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import promptSync from 'prompt-sync';
+const promptSync = require('prompt-sync');
 const prompt = promptSync();
 var i,j,lin,col;
 var jogadas =0;
@@ -13,15 +13,48 @@ for(i = 0; i < 3; i++){
     }
 }
 
-while((jogadas<0)&&(fimJogo==false)){
-    //(b)
+while((jogadas<9)&&(fimJogo==false)){
+    for(i = 0; i < 3; i++){
+    console.log(`[${tab[i][0]}][${tab[i][1]}][${tab[i][2]}]`);
+}
     lin = parseInt(prompt(`${jogador} em [linha]: `));
     col=parseInt(prompt(`${jogador} em [coluna]: `));
-    //(c)
+    if(lin < 0 || lin > 2 || col < 0 || col > 2){
+    console.log("Posicao invalida!");
+}
+else if(tab[lin][col] != ' '){
+    console.log("Posicao ja ocupada!");
+}
+else{
+    tab[lin][col] = jogador;
+    jogadas++;
+
+    //verifica vencedor (8 possibilidades)
+    if(
+        (tab[0][0]==jogador && tab[0][1]==jogador && tab[0][2]==jogador) ||
+        (tab[1][0]==jogador && tab[1][1]==jogador && tab[1][2]==jogador) ||
+        (tab[2][0]==jogador && tab[2][1]==jogador && tab[2][2]==jogador) ||
+        (tab[0][0]==jogador && tab[1][0]==jogador && tab[2][0]==jogador) ||
+        (tab[0][1]==jogador && tab[1][1]==jogador && tab[2][1]==jogador) ||
+        (tab[0][2]==jogador && tab[1][2]==jogador && tab[2][2]==jogador) ||
+        (tab[0][0]==jogador && tab[1][1]==jogador && tab[2][2]==jogador) ||
+        (tab[0][2]==jogador && tab[1][1]==jogador && tab[2][0]==jogador)
+    ){
+        vencedor = jogador;
+        fimJogo = true;
+    }
+    if(!fimJogo){
+    if(jogador == 'o'){
+        jogador = 'x';
+    }else{
+        jogador = 'o';
+    }
+}
+}
 }
 //(d)
 if(vencedor==' '){
-    console.log("empate\n");
+    console.log("Empate\n");
 }else{
     console.log(`${vencedor} venceu!`);
 }
